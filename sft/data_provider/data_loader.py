4285a6bc41ebf94c69964ccc78d9699735fae6c4
zhangmaosen <xiaosen.zhang@gmail.com>
2025-06-16 11:03:32 +0800

    some bug fix

--- sft/data_provider/data_loader.py
+++ sft/data_provider/data_loader.py
@@ -46,16 +46,22 @@ class Dataset_Custom(Dataset):
         
         df_raw = pd.read_parquet(os.path.join(self.root_path,
                                           self.data_path))
-        df_aigc = pd.read_parquet(os.path.join(self.root_path,
-                                          self.aigc_data_path))
-        
-        df_aigc = df_aigc.reindex(df_raw.index)
+        # Handle AIGC data if provided
+        if self.aigc_data_path is not None:
+            df_aigc = pd.read_parquet(os.path.join(self.root_path, self.aigc_data_path))
+            df_aigc = df_aigc.reindex(df_raw.index)
+            self.n_aigc_channels = len(df_aigc.columns[1:].get_level_values(1).unique())
+            aigc_data = df_aigc.values
+        else:
+            # If no AIGC data, create an empty array with zero channels
+            aigc_data = np.zeros((len(df_raw), 0))
+            self.n_aigc_channels = 0
         
         self.assets = df_raw.columns[1:].get_level_values(0).unique().values  
         self.n_assetes = len(self.assets)
         self.channels = df_raw.columns[1:].get_level_values(1).unique()
         self.n_channels = len(self.channels)
-        self.n_aigc_channels = len(df_aigc.columns[1:].get_level_values(1).unique())
+
         '''
         df_raw.columns: ['date', ...(other features), target feature]
         '''
@@ -88,7 +94,6 @@ class Dataset_Custom(Dataset):
         else:
             data = df_data.values
 
-        aigc_data = df_aigc.values
         df_stamp = df_raw[['date']][border1:border2]
         
         
